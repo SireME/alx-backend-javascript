fbe9fdcaa41206ce5062363e8f1a2b5f70ae7cb5
Mbang Ernest N <mbangernest@yahoo.com>
2024-01-09 14:52:06 +0100

    implement input validation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,18 @@
 export default function cleanSet(set, startString) {
+  // Input validation
+  if (!set || !Array.isArray(set)) {
+    throw new Error('The "set" parameter must be a non-null array.');
+  }
+
+  if (typeof startString !== 'string') {
+    throw new Error('The "startString" parameter must be a non-null string.');
+  }
+
   let newString = '';
   const sLen = startString.length;
 
   for (const value of set) {
-    if (value.startsWith(startString)) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
       newString += `${value.slice(sLen)}-`;
     }
   }
