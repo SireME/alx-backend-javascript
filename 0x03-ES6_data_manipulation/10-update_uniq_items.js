ff6ecffb22503c397c98572ad2ec92a28f138686
Mbang Ernest N <mbangernest@yahoo.com>
2024-01-09 15:28:03 +0100

    Map object update

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,15 @@
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  try {
+    map.forEach((value, key) => {
+      if (value === 1) {
+        map.set(key, 100);
+      }
+    });
+  } catch (error) {
+    throw new Error('Cannot process');
+  }
+  return map;
+}
