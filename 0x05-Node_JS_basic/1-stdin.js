8dbbc18135c32627c34ca359ba3458e4fad54373
Mbang Ernest N <mbangernest@yahoo.com>
2024-02-27 17:38:42 +0100

    fix: structure of process

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,12 +2,10 @@ process.stdin.setEncoding('utf8');
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', function (data) {
-  const name = data.trim();
+process.stdin.on('readable', function (data) {
+  const name = process.stdin.read();
   if (name) {
-    process.stdout.write(`Your name is: ${name}\n`);
-  } else {
-    process.stdout.write('Your name is: \n');
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 
