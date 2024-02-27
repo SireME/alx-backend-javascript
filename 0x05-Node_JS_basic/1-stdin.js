3bde08a2b68ad7c1050e642667632ed341ff6986
Mbang Ernest N <mbangernest@yahoo.com>
2024-02-27 17:55:32 +0100

    fix: lint errors

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,12 @@
-process.stdin.setEncoding('utf8');
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', function (data) {
+process.stdin.on('readable', (data) => {
   const name = process.stdin.read();
   if (name) {
     process.stdout.write(`Your name is: ${name}`);
   }
 });
 
-process.stdin.on('end', function () {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
