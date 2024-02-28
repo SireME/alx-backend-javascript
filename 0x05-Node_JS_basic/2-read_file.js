186b07b885bdbaa239fe9a5377adbb8dbffc415f
Mbang Ernest N <mbangernest@yahoo.com>
2024-02-28 02:37:39 +0100

    read file syncronously

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+function countStudents (path) {
+  if (!fs.existsSync(path)) {
+    throw new Error('Cannot load the database');
+  }
+  let csv = fs.readFileSync(path, 'utf-8').split('\n');
+  csv = csv.slice(1);
+  let allStudents = 0;
+  const field = {};
+
+  csv.forEach((value, index) => {
+    allStudents += 1;
+    const vls = value.split(',');
+    const lastIndex = vls.length - 1;
+    if ((field[`${vls[lastIndex]}`]) === undefined) {
+      field[`${vls[lastIndex]}`] = [];
+      field[`${vls[lastIndex]}`].push(vls[0]);
+    } else {
+      field[`${vls[lastIndex]}`].push(vls[0]);
+    }
+  });
+  // log total number of students
+  console.log(`Number of students: ${allStudents}`);
+  for (const key in field) {
+    const list = field[key].join(', ');
+    console.log(`Number of students in  ${key}: ${field[key].length}. List: ${list}`);
+  }
+}
+
+module.exports = countStudents
